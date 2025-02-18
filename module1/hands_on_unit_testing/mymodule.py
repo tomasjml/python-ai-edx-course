c3350cee556c6486ec52c73cacf89e150c249b88
Tomas Molina <jtml.mass@gmail.com>
2025-02-18 15:10:50 -0600

    Add notes and references to course from Module 1 for two Hands on Practice

---
+++ module1/hands_on_unit_testing/mymodule.py
@@ -0,0 +1,20 @@
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a, b):
+    """
+    This function returns the sum of the given numbers
+    :param a: First number to add
+    :param b: Second number to add
+    :return: Sum of the given numbers
+    """
+    return a + b
